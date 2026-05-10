ba16ff6acbdecb3fee82d4af9f9014f8c3f6de67
Urvi Dedhia <urvidedhia.212@gmail.com>
2026-05-10 21:43:14 +0530

    Added PnL Engine.

--- trade-service/src/main/java/com/company/trade/controller/TradeController.java
+++ trade-service/src/main/java/com/company/trade/controller/TradeController.java
@@ -4,10 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.trade.domain.Trade;
+import com.company.trade.dto.PnlResponse;
 import com.company.trade.dto.TradeRequest;
+import com.company.trade.service.PositionService;
 import com.company.trade.service.TradeService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,9 +25,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class TradeController {
 
     private final TradeService tradeService;
+    private final PositionService positionService;
 
     @PostMapping
     public Trade createTrade(@RequestBody TradeRequest tradeRequest) {
         return tradeService.createTrade(tradeRequest);
+    }
+    @GetMapping("/pnl/{commodity}")
+    public PnlResponse getPnL(@PathVariable String commodity) {
+        BigDecimal marketPrice = new BigDecimal("120");
+        return positionService.getPnl(commodity, marketPrice);
     }
 }
--- trade-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO risk_limit (commodity, max_position)
-VALUES ('GOLD', 100);
-
-INSERT INTO risk_limit (commodity, max_position)
-VALUES ('SILVER', 500);
